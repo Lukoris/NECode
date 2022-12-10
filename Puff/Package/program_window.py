e36f9ae888714fdf81c831ec0c8ca88ea7d7fdc7
Lukoris <117269402+Lukoris@users.noreply.github.com>
2022-12-10 15:59:37 +0900

    Update program_window.py

--- Puff/Package/program_window.py
+++ Puff/Package/program_window.py
@@ -9,33 +9,33 @@ from PIL import Image, ImageTk
 __version__ = '0.1'
 
 if __name__ != '__main__':
-    print('Импортирую модуль', __name__)
+    print('Importing a module...', __name__)
 
 
 
-#Окно программы
+#Program window.
 p_window = tk.Tk()
 
 
-#Параметры размеров экрана
+#Screen size options.
 w = p_window.winfo_screenwidth()
 h = p_window.winfo_screenheight()
-#Нахождение середины параметра
+#Finding the middle of the parameter.
 w = w//2
 h = h//2 
-#Подбор по разрешению экрана
+#Selection by screen resolution.
 if w == 960:
-    w = w - 800 # смещение от середины
+    w = w - 800 #Offset from the middle.
     h = h - 400
 else:
     pass
     '''
-    Другие разрешения, не предусмотренны
+    Other permissions not provided.
     '''
     
     
-#Настройка окна программы
-p_window.title('Agnis')  
+#Configuring the program window.
+p_window.title('Puff')  
 p_window.geometry('1402x800+{0}+{1}'.format(w,h))
 p_window.resizable(width=False, height=False)
 p_window.configure(bg='black')
@@ -43,9 +43,9 @@ p_window.configure(bg='black')
 try:
     image = Image.open('redheadandwolf.png')
 except:
-    print('Изображение для фона программы, не было найдено.')
+    print('An image for the background of the program was not found.')
 else:
-    #Назначение фона программы
+    #Assigning the background of the program.
     background_image = ImageTk.PhotoImage(image, master=p_window)
     background = tk.Label(p_window, image = background_image)
     background.image = image
